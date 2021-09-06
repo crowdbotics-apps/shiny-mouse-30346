c525b9fb06acde88a28b15530ad918988748d74c
Crowdbotics <team@crowdbotics.com>
2021-09-06 07:18:39 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,6 @@
+export const globalOptions = {
+  name: "shiny_mouse_30346Identifier",
+  url: "https://shiny_mouse_30346Identifier.botics.co",
+  api: "https://shiny_mouse_30346Identifier.botics.co/api/v1"
+}
+export const modulesOptions = {}
